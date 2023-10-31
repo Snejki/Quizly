a50985af672abb08c7bb56474297780771e892ad
Tomek <tomaszw1995@gmail.com>
2023-10-31 21:44:07 +0100

    feat: add chakra ui

--- frontend/apps/application/app/auth/register/page.tsx
+++ frontend/apps/application/app/auth/register/page.tsx
@@ -1,9 +1,46 @@
+'use client';
+
+import {
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
 import React from 'react';
 
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = () => {
-  return <div>page</div>;
+  const isError = true;
+  const input = '';
+  const handleInputChange = () => {};
+
+  return (
+    <Center>
+      <FormControl isInvalid={isError} size="md" width={'50%'}>
+        <FormLabel>Email</FormLabel>
+        <Input type="email" value={input} onChange={handleInputChange} />
+        {!isError ? (
+          <FormHelperText>
+            Enter the email you'd like to receive the newsletter on.
+          </FormHelperText>
+        ) : (
+          <FormErrorMessage>Email is required.</FormErrorMessage>
+        )}
+        <FormLabel>Password</FormLabel>
+        <Input type="password" value={input} onChange={handleInputChange} />
+        {!isError ? (
+          <FormHelperText>
+            Enter the email you'd like to receive the newsletter on.
+          </FormHelperText>
+        ) : (
+          <FormErrorMessage>Email is required.</FormErrorMessage>
+        )}
+      </FormControl>
+    </Center>
+  );
 };
 
 export default Index;
