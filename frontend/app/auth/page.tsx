e75b7dfb89447d9490b41e9fbdce6accec785ab0
Tomek <tomaszw1995@gmail.com>
2023-11-01 01:33:20 +0100

    feat: get rid of nx

---
+++ frontend/app/auth/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import InputControlledField from "@/lib/forms/InputControlled";
+import { Button, Center, FormControl } from "@chakra-ui/react";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+
+const Login = () => {
+  const methods = useForm({
+    mode: "onChange",
+  });
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <>
+      <Center>
+        <FormProvider {...methods}>
+          <FormControl>
+            <InputControlledField
+              name="email"
+              label="Email"
+              type="text"
+              rules={{ validate: { isValid: (value: string) => "ERROR" } }}
+            />
+            <InputControlledField
+              name="password"
+              label="Password"
+              type="password"
+            />
+            <Button colorScheme="blue" onClick={methods.handleSubmit(onSubmit)}>
+              Submit
+            </Button>
+          </FormControl>
+        </FormProvider>
+      </Center>
+    </>
+  );
+};
+
+export default Login;
--- frontend/apps/application/theme/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Montserrat } from 'next/font/google';
-const montserrat = Montserrat({ subsets: ['latin'] });
-
-/** App Fonts */
-export const fonts = {
-  body: montserrat.style.fontFamily,
-  heading: montserrat.style.fontFamily,
-};
